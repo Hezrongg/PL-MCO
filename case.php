fb98de769e11a9f896833677946707d95f92fddc
HEZRON GAGARIN BSCS2A <hezrongg26@gmail.com>
2025-10-07 19:24:04 +0800

    Add files via upload

---
+++ case.php
@@ -0,0 +1,31 @@
+<?php
+
+$day = "N";
+
+switch ($day) {
+    case 'Monday':
+        echo "Start of the week";
+        break;
+     case 'Tuesday':
+        echo "Second Day";
+        break;
+    case 'Wednesday':
+        echo "Third day";
+        break;
+    case 'Thursday':
+        echo "Fourth Day";
+        break;
+    case 'Friday':
+        echo "Fifth";
+        break;
+    case 'Saturday':
+        echo "Sixth day";
+        break;
+    case 'Sunday':
+        echo "Last Day";
+        break;
+    default:
+        echo "Nothing";
+        break;
+}
+?>
