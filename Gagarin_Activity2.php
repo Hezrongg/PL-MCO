acfc5ad2e5e3e152e39aa6b7dc51502f4eac9f81
HEZRON GAGARIN BSCS2A <hezrongg26@gmail.com>
2025-10-07 19:34:27 +0800

    Gagarin_Activity2.php

--- Gagarin_Activity2.php
+++ Gagarin_Activity2.php
@@ -5,11 +5,12 @@ $weather = "Windy";
 if ($weather == "Sunny") {
     echo "Its a great day! Wear Sunglasses";
 } elseif ($weather == "Rainy") {
-       echo "Bring an umbrella before goint out";
+       echo "Bring an umbrella before going out";
 } elseif ($weather == "Cloudy") {
-    echo "You might still need Jacket";
+    echo "You might still need a light Jacket";
 } elseif ($weather == "Windy") {
-    echo "Hold on to your Hat its Windy";
+    echo "Hold on to your Hat - it's windy!";
 }
+
 
 ?>
