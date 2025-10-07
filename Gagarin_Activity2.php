a9f62e4a0c8798e5dcd6c452de03b8752fca7508
HEZRON GAGARIN BSCS2A <hezrongg26@gmail.com>
2025-10-07 19:32:41 +0800

    Add files via upload

---
+++ Gagarin_Activity2.php
@@ -0,0 +1,15 @@
+<?php
+
+$weather = "Windy";
+
+if ($weather == "Sunny") {
+    echo "Its a great day! Wear Sunglasses";
+} elseif ($weather == "Rainy") {
+       echo "Bring an umbrella before goint out";
+} elseif ($weather == "Cloudy") {
+    echo "You might still need Jacket";
+} elseif ($weather == "Windy") {
+    echo "Hold on to your Hat its Windy";
+}
+
+?>
